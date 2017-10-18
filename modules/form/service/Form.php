f933a16ffe7440daa328c0892d9e2712e648f20b
Iqbal Fauzi <iqbalfawz@gmail.com>
2017-10-18 15:22:40 +0700

    Fix minor bug on unique condition

--- modules/form/service/Form.php
+++ modules/form/service/Form.php
@@ -149,7 +149,7 @@ class Form {
                     continue;
                 
                 $vld_msg = $vld['message'];
-                $vld_arg = array_merge(['field' => $args['label']], $vld_opts);
+                $vld_arg = array_merge($vld_opts, ['field' => $args['label']]);
                 
                 foreach($vld_arg as $plc => $plk){
                     if(is_string($plk) || is_int($plk))
